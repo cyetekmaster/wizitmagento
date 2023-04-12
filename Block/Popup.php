5bd5a434e15464ff91f9afe592a11a8f841dec3f
liuyu0723 <liuyu0723@users.noreply.github.com>
2023-04-12 15:19:32 +1200

    fixed popup issues

--- Block/Popup.php
+++ Block/Popup.php
@@ -1,24 +1,24 @@
 <?php
 namespace Wizit\Wizit\Block;
 
-use Magento\Framework\View\Asset\Repository;
+//use Magento\Framework\View\Asset\Repository;
 
 class Popup extends \Magento\Framework\View\Element\Template
 {
 
-    private $assetRepository;
+    // private $assetRepository;
 
-    public function __construct(
-        Repository $assetRepository
-    ) {
-        $this->assetRepository = $assetRepository;
-    }
+    // public function __construct(
+    //     Repository $assetRepository
+    // ) {
+    //     $this->assetRepository = $assetRepository;
+    // }
 
 
     public function getContent() : string
     {
 
-        $banktransferLogoUrl = $this->assetRepository->getUrlWithParams('Wizit_Wizit::images/wizit_popup.png', []);
+        //$banktransferLogoUrl = $this->assetRepository->getUrlWithParams('Wizit_Wizit::images/wizit_popup.png', []);
 
 
         return 'test popup';
