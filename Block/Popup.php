4087658aefe605282a4ddae276e2eaa73e9f0419
liuyu0723 <liuyu0723@users.noreply.github.com>
2023-04-12 13:49:06 +1200

    clear code

--- Block/Popup.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Wizit\Wizit\Block;
-class Popup extends \Magento\Framework\View\Element\Template
-{
-    public function getContent() : string
-    {
-        return 'test popup';
-    }
-}
