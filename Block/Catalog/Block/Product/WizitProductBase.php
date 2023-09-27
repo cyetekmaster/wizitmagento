0d6f1dafbc0ed77dea7529c34cbae571cd1ad047
liuyu0723 <liuyu0723@users.noreply.github.com>
2023-09-27 16:22:31 +1300

    fixed CPD issues.

--- Block/Catalog/Block/Product/WizitProductBase.php
+++ Block/Catalog/Block/Product/WizitProductBase.php
@@ -20,50 +20,48 @@ class WizitProductBase
         $this->assetRepository = $assetRepository;
     }
 
-
+    private $reg_pricing_code = 'regular_price';
+    private $final_pricing_code = 'final_price';
+    private $product_type_conf = 'configurable';
+    private $product_type_bund = 'bundle';
+    private $product_type_gro = 'grouped';
+    
 
     public function aroundGetProductDetailsHtml(
         $subject,
         $proceed,
-        \Magento\Catalog\Model\Product 	$product
+        \Magento\Catalog\Model\Product 	$selectedProd
     )
-    {
+    {        
+        $p_max_price = 9999; $p_min_price = 0;
+        // Get price if Simple Product
+        $price = $selectedProd->getPriceInfo()->getPrice($this->reg_pricing_code)->getValue();
 
-        $price = $product->getPrice();
-        $min_price = 0;
-        $max_price = 9999;
-        
-        
-        // Simple Product
-        $price = $product->getPriceInfo()->getPrice('regular_price')->getValue();
-
-
-        // Configurable product
-        if ($product->getTypeId() == 'configurable') {
-            $basePrice = $product->getPriceInfo()->getPrice('regular_price');              
-            $price = $basePrice->getMinRegularAmount()->getValue();
+        // Get price if Configurable product
+        if ($selectedProd->getTypeId() == $this->product_type_conf) {
+            $regular_price = $selectedProd->getPriceInfo()->getPrice($this->reg_pricing_code);              
+            $price = $regular_price->getMinRegularAmount()->getValue();
         }
 
-        // Bundle product
-        if ($product->getTypeId() == 'bundle') {
-            $price = $product->getPriceInfo()->getPrice('regular_price')->getMinimalPrice()->getValue();
-            $min_price = $product->getPriceInfo()->getPrice('final_price')->getMinimalPrice()->getValue(); 
-            $max_price = $product->getPriceInfo()->getPrice('final_price')->getMaximalPrice()->getValue();             
+        // Get price if Bundle product
+        if ($selectedProd->getTypeId() == $this->product_type_bund) {
+            $price = $selectedProd->getPriceInfo()->getPrice($this->reg_pricing_code)->getMinimalPrice()->getValue();
+            $p_min_price = $selectedProd->getPriceInfo()->getPrice($this->final_pricing_code)->getMinimalPrice()->getValue(); 
+            $p_max_price = $selectedProd->getPriceInfo()->getPrice($this->final_pricing_code)->getMaximalPrice()->getValue();             
         }
 
-
-        // Group product
-        if ($product->getTypeId() == 'grouped') {
-            $usedProds = $product->getTypeInstance(true)->getAssociatedProducts($product);            
-            foreach ($usedProds as $child) {
-                if ($child->getId() != $product->getId()) {
-                        $price += $child->getPrice();
+        // Get price if Group product
+        if ($selectedProd->getTypeId() == $this->product_type_gro) {
+            $group_prods = $selectedProd->getTypeInstance(true)->getAssociatedProducts($selectedProd);            
+            foreach ($group_prods as $p) {
+                if ($p->getId() != $selectedProd->getId()) {
+                        $price += $p->getPrice();
                 }
             }
         }
 
         
-        return $this->wizit_helper->getWizitMessage('List', $price, $this->assetRepository, $min_price, $max_price);    
+        return $this->wizit_helper->getWizitMessage('List', $price, $this->assetRepository, $p_min_price, $p_max_price);    
 
     }
 }
